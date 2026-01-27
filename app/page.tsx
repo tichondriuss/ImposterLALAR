71797bf7dd3cb71b915c76ef77e83171df3ff5bf
Boldchingis <bboldchingis@gmail.com>
2026-01-27 20:23:31 +0800

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -179,7 +179,7 @@ export default function Home() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.7 }}
-        >
+        > Developed by Boldchingis
         </motion.div>
       </motion.div>
     </div>
